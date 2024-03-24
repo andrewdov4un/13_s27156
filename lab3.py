3f8c8e81309526a40ad9f16e2c7ec36ecb006ef3
andrewdov4un <andrewdov4un@gmail.com>
2024-03-24 22:29:54 +0100

    Task 4

--- lab3.py
+++ lab3.py
@@ -1,17 +1,23 @@
-# Task 3
+import math
+
+# Task 4
 
 class SquareGenerator:
     def generate_squares(self, start, end):
-        """
-        Generate a list of squares for the range of numbers from start to end (inclusive).
-        """
+
         squares = [x**2 for x in range(start, end + 1)]
         return squares
 
-# Example usage of the SquareGenerator class
+    def calculate_square_roots(self, numbers):
+
+        square_roots = [math.sqrt(x) for x in numbers]
+        return square_roots
+
 generator = SquareGenerator()
 start = 1
 end = 10
-result = generator.generate_squares(start, end)
-print("List of squares from", start, "to", end, ":", result)
+squares = generator.generate_squares(start, end)
+print("List of squares from", start, "to", end, ":", squares)
 
+square_roots = generator.calculate_square_roots(squares)
+print("Square roots of the generated squares:", square_roots)
