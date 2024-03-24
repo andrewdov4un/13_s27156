9217de385970c22fe9208501ee853aede756fa27
andrewdov4un <andrewdov4un@gmail.com>
2024-03-24 22:28:50 +0100

    Task3

--- lab3.py
+++ lab3.py
@@ -1,11 +1,17 @@
-# Task 2
+# Task 3
 
-def generate_squares(start, end):
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        """
+        Generate a list of squares for the range of numbers from start to end (inclusive).
+        """
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
 
-    squares = [x**2 for x in range(start, end + 1)]
-    return squares
-
+# Example usage of the SquareGenerator class
+generator = SquareGenerator()
 start = 1
 end = 10
-result = generate_squares(start, end)
+result = generator.generate_squares(start, end)
 print("List of squares from", start, "to", end, ":", result)
+
